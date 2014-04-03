9aebc293b86384ddec15f6881673bfec6263cfb1
Kannan Manickam <kannan@rightscale.com>
2014-04-03 11:36:11 -0700

    It is resource_uid, not href.

--- test/cookbooks/test-rightscale_backup/libraries/helper.rb
+++ test/cookbooks/test-rightscale_backup/libraries/helper.rb
@@ -123,7 +123,7 @@ module RightscaleBackupTest
       filter.merge!({:instance_href => api_client.get_instance.href})
       attachments = api_client.volume_attachments.index(:filter => build_filters(filter))
       # Skip the boot device attachments if there were any
-      attachments.reject { |attachment| attachment.href =~ /\/disks\/boot-/ }
+      attachments.reject { |attachment| attachment.resource_uid =~ /\/disks\/boot-/ }
     end
 
     # Checks if the backup was created in the cloud.
